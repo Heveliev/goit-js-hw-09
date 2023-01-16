05a634c18ef000366e68e3bf49174e4c4b649444
Heveliev-destress <tolik05.03@ukr.net>
2023-01-16 15:30:06 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+const bodyHtml = document.body;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
+
+btnStop.toggleAttribute('disabled');
+
+btnStart.addEventListener('click', onStart)
+function onStart(){
+    btnStart.toggleAttribute('disabled');
+    btnStop.removeAttribute('disabled');
+
+ timerId = setInterval(()=>{
+        bodyHtml.style.backgroundColor = `${getRandomHexColor()}`},1000);
+  
+};
+
+btnStop.addEventListener('click', 
+()=>{
+    btnStart.removeAttribute('disabled');
+    btnStop.toggleAttribute('disabled');
+    clearInterval(timerId);
+})
+
+
+
 
